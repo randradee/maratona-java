eac2b4b382d3887536b30a033279d246c667d4c9
Renato Andrade <dev.renatopandrade@gmail.com>
2022-05-16 20:59:55 -0300

    casting in variables

---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+
+        int idade = (int)10000000000000L;
+        short idadeShort = 10;
+        long numeroGrande = 100000;
+        float salarioFloat = (float)2500D;
+        double salarioDouble = 2000;
+        byte idadeByte = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+
+        System.out.println(idade);
+    }
+}
