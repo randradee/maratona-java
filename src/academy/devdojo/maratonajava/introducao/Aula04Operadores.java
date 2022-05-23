6ee3f2012657d9b9f8be843f0f4e8729b3ee96e4
Renato Andrade <dev.renatopandrade@gmail.com>
2022-05-23 20:28:44 -0300

    aula 04 - operadores finalizada

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -1,5 +1,7 @@
 package academy.devdojo.maratonajava.introducao;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Aula04Operadores {
     public static void main(String[] args) {
         int n1 = 10;
@@ -24,5 +26,41 @@ public class Aula04Operadores {
         System.out.println("isDezIgualADez " + isDezIgualADez);
         System.out.println("isDezDiferenteDeDez " + isDezDiferenteDeDez);
 
+        // operadores lógicos: AND(&&) OR(||) NOT(!)
+        int idade = 32;
+        float salario = 3500;
+        boolean isDentroDaLeiMaiorQueTrinta = idade > 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+        System.out.println("isDentroDaLeiMaiorQueTrinta "+isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta "+isDentroDaLeiMenorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPS5 = 5000;
+
+        boolean isPS5Compravel = valorTotalContaPoupanca > valorPS5 || valorTotalContaCorrente > valorPS5;
+        System.out.println("isPS5Compravel "+isPS5Compravel);
+
+        // atribuição =, +=, -=, *=, /=, %=
+        double bonus = 1800;
+        bonus += 1000;
+        System.out.println("bonus "+bonus);
+        bonus *= 2;
+        System.out.println("bonus "+bonus);
+        bonus -= 1000;
+        System.out.println("bonus "+bonus);
+        bonus /= 2;
+        System.out.println("bonus "+bonus);
+        bonus %= 2;
+        System.out.println("bonus "+bonus);
+
+        // ++ --
+        int contador = 0;
+        contador +=1;
+        contador ++;
+        contador --;
+        ++contador;
+        --contador;
+        System.out.println("contador "+contador);
     }
 }
