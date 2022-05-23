9d5b33519a59c4b984746fe7459ebac99577b33c
Renato Andrade <dev.renatopandrade@gmail.com>
2022-05-23 20:43:38 -0300

    aula 05 inicio

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args) {
+        int idade = 30;
+        if(idade >= 18){
+            System.out.println("Pode comprar cerveja");
+        }else{
+            System.out.printf("Nao pode comprar cerveja");
+        }
+    }
+}
