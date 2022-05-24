dfc82a657e4158ae73be64ccf533711f34e2530b
Renato Andrade <dev.renatopandrade@gmail.com>
2022-05-23 22:07:32 -0300

    aula 06 exercicio

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao02.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao02 {
+    public static void main(String[] args) {
+        // Imprima todos os números pares de 0 até 1_000_000
+        for (int i = 0; i <= 1_000_000; i+=2){
+            System.out.println(i);
+        }
+    }
+}
