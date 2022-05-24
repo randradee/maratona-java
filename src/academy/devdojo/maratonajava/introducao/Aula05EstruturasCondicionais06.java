15904f090f30dab05f37e420f28fe318d8960396
Renato Andrade <dev.renatopandrade@gmail.com>
2022-05-23 21:52:10 -0300

    refatorando exercicio aula 05 switch

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais06.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais06.java
@@ -12,31 +12,47 @@ public class Aula05EstruturasCondicionais06 {
 
         String saida;
 
+//        switch(diaSemana){
+//            case 1:
+//                saida = "Domingo é final de semana";
+//                break;
+//            case 2:
+//                saida = "Segunda é dia útil";
+//                break;
+//            case 3:
+//                saida = "Terça é dia útil";
+//                break;
+//            case 4:
+//                saida = "Quarta é dia útil";
+//                break;
+//            case 5:
+//                saida = "Quinta é dia útil";
+//                break;
+//            case 6:
+//                saida = "Sexta é dia útil";
+//                break;
+//            case 7:
+//                saida = "Sábado é final de semana";
+//                break;
+//            default:
+//                saida = "Dia inválido";
+//        }
         switch(diaSemana){
             case 1:
-                saida = "Domingo é final de semana";
+            case 7:
+                saida = "FDS";
                 break;
             case 2:
-                saida = "Segunda é dia útil";
-                break;
             case 3:
-                saida = "Terça é dia útil";
-                break;
             case 4:
-                saida = "Quarta é dia útil";
-                break;
             case 5:
-                saida = "Quinta é dia útil";
-                break;
             case 6:
-                saida = "Sexta é dia útil";
-                break;
-            case 7:
-                saida = "Sábado é final de semana";
-                break;
+                saida = "Dia útil";
             default:
                 saida = "Dia inválido";
         }
+
+
         System.out.printf(saida);
     }
 }
