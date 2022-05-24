263827b4952a6c8d091aeb72063dfafbb2e04c45
Renato Andrade <dev.renatopandrade@gmail.com>
2022-05-23 22:11:40 -0300

    aula 06 parte 03 break

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao03.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao03 {
+    public static void main(String[] args) {
+        // interrompendo laços
+        for (int i = 0; i < 50; i++) {
+            if (i > 25) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
