a25465af8510eb05cee596a0d33fc6c7e5917ebd
Renato Andrade <dev.renatopandrade@gmail.com>
2022-05-31 13:49:45 -0300

    fazendo coisa errada sem deixar o codigo coeso

--- src/academy/devdojo/maratonajava/javacore/Aintrocucaoclasses/dominio/Estudante.java
+++ src/academy/devdojo/maratonajava/javacore/Aintrocucaoclasses/dominio/Estudante.java
@@ -4,4 +4,10 @@ public class Estudante {
     public String nome = "Zoro"; // nome padrão
     public int idade;
     public char sexo;
+
+    // O código tem que estar coeso! Isto aqui é errado!!!!!!!!
+    // O correto seria criar uma classe Professor
+    public String nomeProfessor;
+    public int idadeProfessor;
+    public char sexoProfessor;
 }
